e68dcc3ed90d993adcdcb7221188b328b0858a99
Devinix00 <dpldpl123456@naver.com>
2024-04-12 18:16:00 +0900

    design: 로그인 페이지 글 추가

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,6 +6,10 @@ function SignIn() {
     <div className="w-[100%] min-h-[100vh] bg-black flex flex-col justify-center items-center">
       <h1 className="text-white mb-4 text-2xl font-medium">Sign In</h1>
       <SignInForm />
+      <p className="w-80 text-gray-400 mt-5 text-center">
+        Please enter any values in the sign in inputs. Even if you enter an
+        arbitrary value, you will be able to log in successfully!
+      </p>
     </div>
   );
 }
